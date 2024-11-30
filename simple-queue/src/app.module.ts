eb16653d71a9024dd3f49c610ae415ac243249b1
meshach567 <arinzemeshach123@gmail.com>
2024-11-30 16:34:47 +0100

    worked on the user entity for simple-queue

---
+++ simple-queue/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import databaseConfig from 'config/database.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234567890',
+      database: 'simple-message-queue',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ simple-queue/src/users/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString, IsEmail} from 'class-validator'
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    username: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsEmail()
+    email: string;
+
+}
